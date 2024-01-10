2823f4411cbc68f540a1e86fbb9f68f58add5990
aishwarya kademani <aishwaryagk2023@gmail.com>
2024-01-11 00:50:31 +0530

    files

---
+++ ConsoleApp1/ConsoleApp1/FileExistOperationWithTextF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace FileBasicOperations
+{
+    public class FileExistOperationWithTextF
+    {
+        public void FindFileExist()
+        {
+            string path = @"D:\visual studio C# projects\ConsoleApp1\ConsoleApp1\Demo.txt";
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File exists");
+            }
+            else
+            {
+                Console.WriteLine("File doesnot exist");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using FileBasicOperations;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //foreach (int i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine();
+            //var ab=(from t in arr where t>5 select t);
+            //foreach (int i in ab)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //ArrayList a= new ArrayList();
+            //a.Add(1);
+            //a.Add(2);
+            //a.Add(3);
+            //Console.WriteLine();
+
+            //foreach (int i in a)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            FileExistOperationWithTextF ob = new FileExistOperationWithTextF();
+            ob.FindFileExist();
+        }
+    }
+}
