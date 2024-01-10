19b51fdeb48884ea0787c14c6ce185bb30994231
aishwarya kademani <aishwaryagk2023@gmail.com>
2024-01-10 22:28:24 +0530

    commited

---
+++ BasicProgram/PosNeg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    public class PosNeg
+    {
+        public void Findposneg()
+        {
+            Console.WriteLine("Enter a number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            if (num < 0)
+            {
+                Console.WriteLine("number is negative");
+            }
+            else
+            {
+                Console.WriteLine("number is positive");
+            }
+        }
+    }
+}
---
+++ BasicProgram/Prime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicProgram
+{
+    public class Prime
+    {
+        public void Primenumber()
+        {
+            Boolean flag = true;
+            Console.WriteLine("enter number");
+            int num = int.Parse(Console.ReadLine());
+            for(int i = 0; i < num; i++)
+            {
+                if(num%i==0)
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag==true)
+            {
+                Console.WriteLine(num + " is prime number");
+            }
+            else 
+            { 
+                Console.WriteLine(num + " is not prime number");
+            }
+
+        }
+    }
+}
